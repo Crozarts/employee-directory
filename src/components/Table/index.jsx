502a4bd66520ac0ed9ff8740cda421d1d360381a
Crozarts <imarcrosby@optimum.net>
2021-01-20 22:02:09 -0500

    tried to add container but app still works table is just not center yet

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './style.css';
 import axios from 'axios';
-import Search from '../Form/index'
+import Search from '../Form/index';
+import Container from '../Container'
 
 
 class Table extends React.Component {
@@ -48,44 +49,46 @@ class Table extends React.Component {
     render() {
         return (
             <div>
-                <Search search={this.state.search} handleInputChange={this.handleInputChange} />
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Image</th>
-                            <th>Name</th>
-                            <th>Phone</th>
-                            <th>Email</th>
-                            <th>DOB</th>
-                            <button onClick={() => this.sortByName()}>Sort by name</button>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.users
-                            .filter(
-                                (user) =>
-                                    user.name.first
-                                        .toLowerCase()
-                                        .includes(this.state.search.toLowerCase()) ||
-                                    user.name.last
-                                        .toLowerCase()
-                                        .includes(this.state.search.toLowerCase())
-                            )
-                            .map((user) => (
-                                <tr key={user.name}>
-                                    <td>
-                                        <img src={user.picture.thumbnail} alt=""></img>
-                                    </td>
-                                    <td>
-                                        {user.name.first} {user.name.last}
-                                    </td>
-                                    <td>{user.phone}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.dob.date}</td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </table>
+                <Container>
+                    <Search search={this.state.search} handleInputChange={this.handleInputChange} />
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Image</th>
+                                <th>Name</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                                <th>DOB</th>
+                                <button onClick={() => this.sortByName()}>Sort by name</button>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.users
+                                .filter(
+                                    (user) =>
+                                        user.name.first
+                                            .toLowerCase()
+                                            .includes(this.state.search.toLowerCase()) ||
+                                        user.name.last
+                                            .toLowerCase()
+                                            .includes(this.state.search.toLowerCase())
+                                )
+                                .map((user) => (
+                                    <tr key={user.name}>
+                                        <td>
+                                            <img src={user.picture.thumbnail} alt=""></img>
+                                        </td>
+                                        <td>
+                                            {user.name.first} {user.name.last}
+                                        </td>
+                                        <td>{user.phone}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.dob.date}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </Container>
             </div>
         );
     }
--- src/pages/Directory.jsx
+++ src/pages/Directory.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header/index.jsx';
 import Table from '../components/Table/index.jsx';
 
 
+
 function Directory() {
 
     return (
